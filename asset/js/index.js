3bf8ba774c62001b6c6b7858076fb01deb0aebcb
yaw595 <39474651+yaw595@users.noreply.github.com>
2020-06-27 11:55:00 +0000

    side nav added for mobile

--- asset/js/index.js
+++ asset/js/index.js
@@ -3,23 +3,17 @@ $('.carousel').carousel({
   })
 
 
- /* $(document).ready(function () {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items: 8,
-        loop: true,
-        margin: 20,
-        autoplay: true,
-        slideTransition: 'linear',
-        autoplayTimeout: 0,
-        autoplaySpeed: 3000,
-        autoplayHoverPause: false
-
-    });*/
-
-
 $(document).ready(function() {
   $("nav").click(function() {
     $(this).toggleClass("_expand");
   });
-});
+}); 
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
