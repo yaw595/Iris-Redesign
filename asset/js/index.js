62d2db3a61d8fd0be7179770fcb90b44dee74a08
yaw595 <39474651+yaw595@users.noreply.github.com>
2020-07-02 14:54:03 +0000

    fixed recent works

--- asset/js/index.js
+++ asset/js/index.js
@@ -3,17 +3,44 @@ $('.carousel').carousel({
   })
 
 
-$(document).ready(function() {
-  $("nav").click(function() {
-    $(this).toggleClass("_expand");
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 100) {
+      $(".top-stuff").css('background-color', 'white');
+    } else {
+      $(".top-stuff").css('background-color', 'transparent');
+    }
   });
 }); 
 
+console.clear();
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
+const app = (() => {
+  let body;
+  let menu;
+  let menuItems;
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+  const init = () => {
+    body = document.querySelector('body');
+    menu = document.querySelector('.menu-icon');
+    menuItems = document.querySelectorAll('.nav__list-item');
+
+    applyListeners();
+  }
+
+  const applyListeners = () => {
+    menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+  }
+
+  const toggleClass = (element, stringClass) => {
+    if (element.classList.contains(stringClass))
+      element.classList.remove(stringClass);
+    else
+      element.classList.add(stringClass);
+  }
+
+  init();
+})();
+
