63079476c9243caaaac2492e984053d3fcf7f1b4
Kennedy Ofosu <kennedyofosu64@gmail.com>
2020-07-08 10:15:51 +0000

    updates on additional menu
    linked all menu
    setup potforlio page
    potforlio detail page

--- asset/js/sign-in.js
+++ asset/js/sign-in.js
@@ -1,12 +1,46 @@
-
 /***********************
  * Navbar Menu expand
  */
-$(document).ready(function() {
-    $("nav").click(function() {
-      $(this).toggleClass("_expand");
-    });
+$(document).ready(function () {
+	var scroll_pos = 0;
+	$(document).scroll(function () {
+		scroll_pos = $(this).scrollTop();
+		if (scroll_pos > 100) {
+			$(".top-stuff").css('background-color', 'white');
+		} else {
+			$(".top-stuff").css('background-color', 'transparent');
+		}
+	});
 });
+
+console.clear();
+
+const app = (() => {
+	let body;
+	let menu;
+	let menuItems;
+
+	const init = () => {
+		body = document.querySelector('body');
+		menu = document.querySelector('.menu-icon');
+		menuItems = document.querySelectorAll('.nav__list-item');
+
+		applyListeners();
+	}
+
+	const applyListeners = () => {
+		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+	}
+
+	const toggleClass = (element, stringClass) => {
+		if (element.classList.contains(stringClass))
+			element.classList.remove(stringClass);
+		else
+			element.classList.add(stringClass);
+	}
+
+	init();
+})();
 
 
 /*********************
@@ -15,14 +49,14 @@ $(document).ready(function() {
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
+function addcl() {
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
-function remcl(){
+function remcl() {
 	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
+	if (this.value == "") {
 		parent.classList.remove("focus");
 	}
 }
@@ -32,7 +66,3 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
-
-
-
-
--- asset/js/sign-up.js
+++ asset/js/sign-up.js
@@ -2,11 +2,46 @@
 /***********************
  * Navbar Menu expand
  */
-$(document).ready(function() {
-    $("nav").click(function() {
-      $(this).toggleClass("_expand");
-    });
-});
+$(document).ready(function () {
+	var scroll_pos = 0;
+	$(document).scroll(function () {
+	  scroll_pos = $(this).scrollTop();
+	  if (scroll_pos > 100) {
+		$(".top-stuff").css('background-color', 'white');
+	  } else {
+		$(".top-stuff").css('background-color', 'transparent');
+	  }
+	});
+	}); 
+	
+	console.clear();
+	
+	const app = (() => {
+	let body;
+	let menu;
+	let menuItems;
+	
+	const init = () => {
+	  body = document.querySelector('body');
+	  menu = document.querySelector('.menu-icon');
+	  menuItems = document.querySelectorAll('.nav__list-item');
+	
+	  applyListeners();
+	}
+	
+	const applyListeners = () => {
+	  menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+	}
+	
+	const toggleClass = (element, stringClass) => {
+	  if (element.classList.contains(stringClass))
+		element.classList.remove(stringClass);
+	  else
+		element.classList.add(stringClass);
+	}
+	
+	init();
+	})();
 
 
 /*********************
